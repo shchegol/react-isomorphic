2643762b56d951d679ea6998f137fc42176d4202
Alexander Shchegol <zelenzoom@gmail.com>
2017-08-11 00:11:28 +0300

    react-isomorphic / add simple webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,45 +1,66 @@
-const path = require('path');
+global.Promise = require('bluebird');
+
 const webpack = require('webpack');
+const path = require('path');
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
 const CleanWebpackPlugin = require('clean-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const ExtractTextPlugin = require("extract-text-webpack-plugin");
+
+const publicPath = 'http://localhost:8050/public/assets';
+const cssName = process.env.NODE_ENV === 'production' ? 'styles-[hash].css' : 'styles.css';
+const jsName = process.env.NODE_ENV === 'production' ? 'bundle-[hash].js' : 'bundle.js';
+const plugins = [
+    new webpack.DefinePlugin({
+        'process.env': {
+            BROWSER: JSON.stringify(true),
+            NODE_ENV: JSON.stringify(process.env.NODE_ENV || 'development')
+        }
+    }),
+    new ExtractTextPlugin(cssName),
+    new webpack.LoaderOptionsPlugin({
+        debug: process.env.NODE_ENV !== 'production',
+    }),
+    new CleanWebpackPlugin(['dist']),
+    new HtmlWebpackPlugin({
+        template: 'index.ejs',
+        title: 'React',
+        favicon: '../favicon.ico'
+    }),
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.ProvidePlugin({
+        $: 'jquery',
+        jQuery: 'jquery'
+    }),
+];
+
+if (process.env.NODE_ENV === 'production') {
+    plugins.push(
+        new CleanWebpackPlugin(['public/assets/'], {
+            root: __dirname,
+            verbose: true,
+            dry: false
+        })
+    );
+    plugins.push(new webpack.optimize.DedupePlugin());
+    plugins.push(new webpack.optimize.OccurenceOrderPlugin());
+}
 
 module.exports = {
-    devtool: 'inline-source-map',
-
     context: path.resolve(__dirname, './src'),
-    entry: {
-        app: './index.js'
+    entry: ['babel-polyfill', './client.js'],
+    resolve: {
+        modules: ['node_modules'],
+        extensions: ['.js', '.jsx']
     },
+    plugins,
     output: {
-        filename: '[name].bundle.js',
-        path: path.resolve(__dirname, 'dist')
+        path: `${__dirname}/public/assets/`,
+        filename: jsName,
+        publicPath
     },
-
     module: {
         rules: [
-            {
-                test: /\.(html)$/,
-                use: {
-                    loader: 'html-loader',
-                    options: {
-                        attrs: [':data-src']
-                    }
-                }
-            },
-            {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: 'babel-loader',
-                    options: {
-                        cacheDirectory: true,
-                        presets: ['env'],
-                        plugins: ['transform-runtime']
-                    }
-                }
-            },
-
+            {test: /\.(html)$/, use: 'html-loader'},
             {
                 test: /\.css$/,
                 use: ExtractTextPlugin.extract({
@@ -54,27 +75,28 @@ module.exports = {
                     use: 'css-loader!sass-loader'
                 })
             },
-            {test: /\.woff2?(\?v=[0-9]\.[0-9]\.[0-9])?$/, use: 'url-loader?limit=10000'},
-            {test: /\.(ttf|eot)(\?[\s\S]+)?$/, use: 'file-loader'},
-            {test: /\.(png|svg|jpg|gif)$/, use: ['file-loader']}
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        cacheDirectory: true,
+                        presets: ['env'],
+                        plugins: ['transform-runtime']
+                    }
+                }
+            },
+            {test: /\.jsx?$/, exclude: [/node_modules/, /public/], use: 'babel-loader'},
+            {test: /\.json$/, use: 'json-loader'},
+            {test: /\.(woff|woff2|ttf|eot)/, loader: 'url-loader?limit=1'},
+            {test: /\.gif$/, use: 'url-loader?limit=10000&mimetype=image/gif'},
+            {test: /\.jpg$/, use: 'url-loader?limit=10000&mimetype=image/jpg'},
+            {test: /\.png$/, use: 'url-loader?limit=10000&mimetype=image/png'},
+            {test: /\.svg/, use: 'url-loader?limit=26000&mimetype=image/svg+xml'},
         ]
     },
-
-    plugins: [
-        new CleanWebpackPlugin(['dist']),
-        new HtmlWebpackPlugin({
-            template: 'index.ejs',
-            title: 'React',
-            favicon: '../favicon.ico'
-        }),
-        new ExtractTextPlugin( "bundle.css" ),
-        new webpack.HotModuleReplacementPlugin(),
-        new webpack.ProvidePlugin({
-            $: 'jquery',
-            jQuery: 'jquery'
-        }),
-    ],
-
+    devtool: process.env.NODE_ENV !== 'production' ? 'source-map' : null,
     devServer: {
         hot: true,
         contentBase: path.resolve(__dirname, 'dist'),
